dc3b734cf9d3aa8689a6619e7ea489e6755ff483
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2025-05-29 00:46:18 -0300

    refactor: adopting camelCase in endpoints URLs

--- ashpath-backend/src/main/java/com/pdsc/ashpath/controllers/CremationEntryController.java
+++ ashpath-backend/src/main/java/com/pdsc/ashpath/controllers/CremationEntryController.java
@@ -1,21 +1,25 @@
 package com.pdsc.ashpath.controllers;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.pdsc.ashpath.domain.dto.request.AddDeceasedToCremationEntryRequest;
 import com.pdsc.ashpath.domain.dto.request.CreateCremationEntryRequest;
+import com.pdsc.ashpath.domain.dto.response.CremationEntryResponse;
 import com.pdsc.ashpath.domain.service.CremationEntryService;
 
 import lombok.RequiredArgsConstructor;
 
-import java.util.List;
-
-import com.pdsc.ashpath.domain.dto.response.CremationEntryResponse;
-
 @RestController
-@RequestMapping("/cremation_entry")
+@RequestMapping("/cremationEntry")
 @RequiredArgsConstructor
 public class CremationEntryController
 {
