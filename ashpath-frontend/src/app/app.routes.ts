a14f59585572fe226aa89f249de611efc7141977
albertalvin8080 <albertalvin99@gmail.com>
2025-06-08 16:23:57 -0300

    wip: register component

--- ashpath-frontend/src/app/app.routes.ts
+++ ashpath-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
     { path: "login", component: LoginComponent },
     { path: "**", redirectTo: "/login" },
 ];
