9968593f38fa6bcd23fd7101c57615ce6bcc73ba
Lucas Oliveira <lucas.g.oliveira1998@outlook.com>
2025-06-20 13:29:39 -0300

    refactor: remove unnecessary code

--- ashpath-backend/src/main/java/com/pdsc/ashpath/domain/dto/request/user/CreateAdminUserRequest.java
+++ ashpath-backend/src/main/java/com/pdsc/ashpath/domain/dto/request/user/CreateAdminUserRequest.java
@@ -11,34 +11,4 @@ public class CreateAdminUserRequest extends AbstractCreateUserRequest
     this.password = password;
     this.fullname = fullname;
   }
-
-  public void setEmail(String email)
-  {
-    this.email = email;
-  }
-
-  public String getEmail()
-  {
-    return this.email;
-  }
-
-  public void setPassword(String password)
-  {
-    this.password = password;
-  }
-
-  public String getPassword()
-  {
-    return this.password;
-  }
-
-  public void setFullname(String fullname)
-  {
-    this.fullname = fullname;
-  }
-
-  public String getFullname()
-  {
-    return this.fullname;
-  }
 }
