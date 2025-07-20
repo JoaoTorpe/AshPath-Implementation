ffe993e53795ed26be1d26365744dbb7dc9a8a73
albertalvin8080 <albertalvin99@gmail.com>
2025-07-20 11:40:35 -0300

    chore: front.bat

---
+++ front.bat
@@ -0,0 +1,13 @@
+@echo off
+
+:: Temporarily switch to the ashpath-frontend directory
+pushd ashpath-frontend || (
+    echo Directory 'ashpath-frontend' not found
+    exit /b 1
+)
+
+:: Run the command
+npm start
+
+:: Return to the original directory
+popd
